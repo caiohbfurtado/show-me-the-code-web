63f7aac25608867f60a1a8d84f0878878a871c93
Caio Furtado <62728052+caiohbfurtado@users.noreply.github.com>
2020-08-27 23:49:11 -0300

    add theme ui

---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+const theme = {
+  palette: {
+    primary: {
+      light: '#ffff6b',
+      main: '#fdd835',
+      dark: '#c6a700',
+    },
+    secondary: {
+      light: '#ffff5a',
+      main: '#ffff00',
+      dark: '#c7cc00',
+    },
+    background: {
+      main: '#eee',
+      secondary: '#b1b1b1',
+    },
+  },
+};
+
+export default theme;
